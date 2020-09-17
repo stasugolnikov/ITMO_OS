5fc7d8ef01d983c2577ba119838b2213686710e5
stasugolnikov <ssugol@yandex.ru>
2020-09-17 19:37:45 +0300

    some changes

--- lab1/task_1.sh
+++ lab1/task_1.sh
@@ -1,39 +1,53 @@
 #! /bin/bash
 . ./task_7.sh
 
-function calc {
-	if [[ $# < 3 ]] 
-	then	
-		echo "requires 3 arguments, but $# was provided">&2 
-	       	exit -1	
-	fi
-	if [[ $2 =~ ^-?[[:digit:]]+$ && $3 =~ ^-?[[:digit:]]+$ ]] 
+function check_args {
+	if [[ $# < 3 ]]
 	then
-		case $1 in
-			'sum')
-				echo $[$2 + $3]
-				;;
-			'sub')
-				echo $[$2 - $3]
-				;;
-			'mul')
-			       	echo $[$2 * $3]
-				;;
-			'div') 
-				if [[ "$3" -ne "0" ]]
-		       		then
-	       				echo $[$2 / $3]
-				else 
-					echo "division by zero" >&2
-					exit -1
-				fi
-				;;
-			*)
-				echo 'wrong command' >&2
-				Help
-		esac
-	else
-		echo "$2 and $3 must be integers"
+		echo "requires 3 arguments, but $# was provided">&2
+		exit -1
+	fi
+	if [[ $2 =~ ^-?[[:digit:]]+$ && $3 =~ ^-?[[:digit:]]+$ ]]
+	then
+		echo "arguments must be integers">&2
+		exit -2
+	fi
+	if [[ $1 -eq 'div' && $3 -eq 0 ]]
+	then
+		echo "division by zero">&2
+		exit -3
 	fi
 }
 
+function sum {
+	echo $[$1 + $2]
+}
+function sub {
+	echo $[$1 - $2]
+}
+function mul {
+	echo $[$1 * $2]
+}
+function div {
+	echo $[$1 / $2]
+}
+
+function switch_case {
+	case $1 in
+		'sum')
+			sum $2 $3
+			;;
+		'sub')
+			sub $2 $3
+			;;
+		'mul')
+		        mul $2 $3
+			;;
+		'div') 
+			div $2 $3
+			;;
+		*)
+			echo 'command does not exists'>&2
+			Help
+	esac
+}
